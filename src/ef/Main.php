b23a677cbc591ea892acbf6f7509228d154c36f1
GroupPluginer <4gamesl7o@gmail.com>
2017-07-11 18:19:03 +0300

    Create Main.php

---
+++ src/ef/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ef;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\entity\Effect;
+use pocketmine\text\TextFormat as c;
+
+class Main extends PluginBase{
+
+public function onEnable(){
+  $this->getServer()->getLogger()->info(c::GREEN."plugin is on by GROUPpluginer");
+    $this->getLogger()->info(c::YELLOW."if you want share plugin say is by GroupPLuginer");
+      }
+     public function onDisable(){
+        $this->getServer()->getLogger()->info(c::RED."PLUGIN has been disable");
+         }
+        public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+         if($cmd->getName() == 'cmd'){
+           switch(mt_rand(ef,Ra)){
+            case ef:
+             $Effect = Effect::getEffect();
